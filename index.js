ac3be31ecbe5c469bcfc8bc0606ff3e29b91f7dc
René van Mil <rvanmil@me.com>
2020-05-11 19:47:40 +0200

    additional JSX rules

--- index.js
+++ index.js
@@ -27,6 +27,28 @@ module.exports = {
 			.filter(name => name !== 'self'), // The 'self' global is used by workers, so we remove it from the default restricted globals list
 		'react-hooks/rules-of-hooks': 'error',
 		'react-hooks/exhaustive-deps': 'error',
-		'react/prop-types': [2, { skipUndeclared: true }]
+		'react/jsx-closing-bracket-location': ['error', {
+			nonEmpty: 'after-props',
+			selfClosing: 'line-aligned'
+		}],
+		'react/jsx-closing-tag-location': 'error',
+		'react/jsx-first-prop-new-line': ['error', 'multiline-multiprop'],
+		'react/jsx-indent': ['error', 'tab', {
+			checkAttributes: true,
+			indentLogicalExpressions: true
+		}],
+		'react/jsx-max-props-per-line': ['error', {
+			maximum: 1,
+			when: 'multiline'
+		}],
+		'react/jsx-tag-spacing': ['error', {
+			closingSlash: 'never',
+			beforeSelfClosing: 'always',
+			afterOpening: 'never',
+			beforeClosing: 'never'
+		}],
+		'react/prop-types': ['error', {
+			skipUndeclared: true
+		}]
 	}
 }
